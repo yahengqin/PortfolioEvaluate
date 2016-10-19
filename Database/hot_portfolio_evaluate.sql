050ba04062412e35b71a85c2828c913391a9c473
yahengqin <yahengqin@163.com>
2016-10-19 16:20:04 +0800

    修改SQL脚本
    添加了表的主键信息，删除了部分无用的索引信息；

--- Database/hot_portfolio_evaluate.sql
+++ Database/hot_portfolio_evaluate.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50624
 File Encoding         : 65001
 
-Date: 2016-09-11 11:44:21
+Date: 2016-10-19 16:17:40
 */
 CREATE DATABASE
 IF NOT EXISTS hot_portfolio_evaluate DEFAULT CHARACTER
@@ -27,9 +27,9 @@ DROP TABLE IF EXISTS `t_stock_base_earnings`;
 CREATE TABLE `t_stock_base_earnings` (
   `stock_code` varchar(20) NOT NULL,
   `trade_day` int(10) NOT NULL COMMENT '记录规则如：20160911',
-  `stock_earnings` double DEFAULT NULL,
+  `stock_earnings` double NOT NULL,
   `earnings_type` char(2) DEFAULT NULL COMMENT '0：个股收益率；1：行业收益率；2：沪深300收益率；',
-  KEY `stock_day` (`stock_code`,`trade_day`)
+  PRIMARY KEY (`stock_code`,`trade_day`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -41,7 +41,8 @@ CREATE TABLE `t_stock_fluctuation_ratio` (
   `fluctuation_ratio_20` double DEFAULT NULL,
   `fluctuation_ratio_120` double DEFAULT NULL,
   `fluctuation_ratio_250` double DEFAULT NULL,
-  `fluctuation_ratio_500` double DEFAULT NULL
+  `fluctuation_ratio_500` double DEFAULT NULL,
+  PRIMARY KEY (`stock_code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -53,7 +54,8 @@ CREATE TABLE `t_stock_max_draw_down` (
   `max_draw_down_20` double DEFAULT NULL,
   `max_draw_down_120` double DEFAULT NULL,
   `max_draw_down_250` double DEFAULT NULL,
-  `max_draw_down_500` double DEFAULT NULL
+  `max_draw_down_500` double DEFAULT NULL,
+  PRIMARY KEY (`stock_code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -65,7 +67,8 @@ CREATE TABLE `t_stock_sharp_ratio` (
   `sharp_ratio_20` double DEFAULT NULL,
   `sharp_ratio_120` double DEFAULT NULL,
   `sharp_ratio_250` double DEFAULT NULL,
-  `sharp_ratio_500` double DEFAULT NULL
+  `sharp_ratio_500` double DEFAULT NULL,
+  PRIMARY KEY (`stock_code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -77,7 +80,8 @@ CREATE TABLE `t_stock_sum_earnings` (
   `sum_earnings_20` double DEFAULT NULL,
   `sum_earnings_120` double DEFAULT NULL,
   `sum_earnings_250` double DEFAULT NULL,
-  `sum_earnings_500` double DEFAULT NULL
+  `sum_earnings_500` double DEFAULT NULL,
+  PRIMARY KEY (`stock_code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -86,5 +90,6 @@ CREATE TABLE `t_stock_sum_earnings` (
 DROP TABLE IF EXISTS `t_stock_var_ratio`;
 CREATE TABLE `t_stock_var_ratio` (
   `stock_code` varchar(20) NOT NULL,
-  `var_ratio_250` double DEFAULT NULL
+  `var_ratio_250` double DEFAULT NULL,
+  PRIMARY KEY (`stock_code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
