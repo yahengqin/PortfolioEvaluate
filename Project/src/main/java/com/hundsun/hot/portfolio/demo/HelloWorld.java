8dc27bb9fb36d2a64c7cff8a7ac4e79c1bfc84bb
yahengqin <yahengqin@163.com>
2016-11-16 17:20:47 +0800

    删除无效的引用

--- Project/src/main/java/com/hundsun/hot/portfolio/demo/HelloWorld.java
+++ Project/src/main/java/com/hundsun/hot/portfolio/demo/HelloWorld.java
@@ -1,6 +1,5 @@
 package com.hundsun.hot.portfolio.demo;
 
-import org.apache.tomcat.jni.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
